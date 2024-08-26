94d9a4e9e9f64591a5904f538164c7d9979eb1ed
onukwilip <onukwilip@gmail.com>
2024-08-26 09:43:42 +0100

    Chenged loader from image to text

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,11 +2,7 @@ import Loader from "@/components/Loader";
 import React from "react";
 
 const Loading = () => {
-  return (
-    <>
-      <Loader />
-    </>
-  );
+  return <>Loading...</>;
 };
 
 export default Loading;
